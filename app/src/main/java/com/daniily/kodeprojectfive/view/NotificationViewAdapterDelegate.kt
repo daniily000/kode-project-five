5742473517ff646561b2d940ca156bd33841c2d7
daniily <daniily000@yandex.ru>
2019-01-28 04:54:45 +0200

    Notification delegate

---
+++ app/src/main/java/com/daniily/kodeprojectfive/view/NotificationViewAdapterDelegate.kt
@@ -0,0 +1,50 @@
+package com.daniily.kodeprojectfive.view
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.daniily.kodeprojectfive.R
+import com.daniily.kodeprojectfive.data.FeedBase
+import com.daniily.kodeprojectfive.data.Notification
+import com.hannesdorfmann.adapterdelegates4.AdapterDelegate
+import kotlinx.android.synthetic.main.view_notification.view.*
+
+//
+class NotificationViewAdapterDelegate(activity: Activity) : AdapterDelegate<List<FeedBase>>() {
+
+    private val inflater: LayoutInflater = activity.layoutInflater
+
+    override fun onCreateViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder{
+        return NotificationViewHolder(
+            inflater.inflate(
+                R.layout.view_notification,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun isForViewType(items: List<FeedBase>, position: Int) =
+        items[position] is Notification
+
+    override fun onBindViewHolder(
+        items: List<FeedBase>,
+        position: Int,
+        holder: RecyclerView.ViewHolder,
+        payloads: MutableList<Any>
+    ) {
+        val o = items[position]
+
+        if (o is Notification) holder.itemView.apply {
+            image_notification.setImageResource(o.imageId)
+            title_notification.text = o.title
+            content_notification.text = o.message
+        }
+    }
+
+
+    class NotificationViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+}
