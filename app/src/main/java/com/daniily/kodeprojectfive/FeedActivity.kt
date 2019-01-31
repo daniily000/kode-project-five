f5bb8bcf46fcebb7f7630d0bff0e471b1a87a8d2
daniily <daniily000@yandex.ru>
2019-01-31 21:36:01 +0200

    FeedActivity: feed on DAO

--- app/src/main/java/com/daniily/kodeprojectfive/FeedActivity.kt
+++ app/src/main/java/com/daniily/kodeprojectfive/FeedActivity.kt
@@ -3,8 +3,9 @@ package com.daniily.kodeprojectfive
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.daniily.kodeprojectfive.data.News
-import com.daniily.kodeprojectfive.data.Notification
+import com.daniily.kodeprojectfive.dao.AppDAO
+import com.daniily.kodeprojectfive.dao.AppPrefsDAO
+import com.daniily.kodeprojectfive.data.FeedBase
 import com.daniily.kodeprojectfive.view.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.squareup.picasso.Picasso
@@ -18,40 +19,8 @@ class FeedActivity : AppCompatActivity() {
     private lateinit var newsDelegate: NewsViewAdapterDelegate
     private lateinit var notificationDelegate: NotificationViewAdapterDelegate
 
-    private val feedItems = listOf(
-
-        News(
-            "Looking Glass",
-            "Marcus entered the bright room of secured high-tech state-of-the-art skyscraper and finally got " +
-                    "the desired laptop of !Nvite's CEO. ",
-            R.mipmap.image1
-        ),
-
-        Notification(
-            "Marcus",
-            "I'm in. Trying to figure out who boosted our numbers. Whoa! Whoa! What happened?",
-            R.mipmap.image_marcus
-        ),
-
-        News(
-            "Suddenly...",
-            "The laptop started glitching, ignoring any Marcus' attempt to fix anything.",
-            R.mipmap.image2
-        ),
-
-        Notification(
-            "Sitara",
-            "The flagged clients just vanished! We lost the bots. What did you do?",
-            R.mipmap.image_sitara
-        ),
-
-        Notification(
-            "Marcus",
-            "Nothing! This isn't me, I didn't...",
-            R.mipmap.image_marcus
-        )
-    )
-
+    private lateinit var appDAO: AppDAO
+    private lateinit var feedItems: List<FeedBase>
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -86,6 +55,9 @@ class FeedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_feed)
         setResult(NO_LOGOUT)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        appDAO = AppPrefsDAO(this)
+        feedItems = appDAO.getFeedObjects()
 
         logout.setOnClickListener {
             setResult(LOGOUT)
